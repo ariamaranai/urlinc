d892d55fef39960638593e3471db8dd4bd104ee0
a <106607480+ariamaranai@users.noreply.github.com>
2025-10-30 23:35:27 +0900

    Add files via upload

--- popup.js
+++ popup.js
@@ -2,8 +2,8 @@ onkeyup = e => e.keyCode == 13 && chrome.tabs.query({ active: !0, currentWindow:
   let count = +document.body.lastChild.value;
   if (count) {
     let tab = tabs[0];
-    let { url } = tab;
-    let result =/(?<!%[0-9A-Fa-f]{0,2})\d+(?!.*?(?<!%[0-9A-Fa-f]{0,2})\d)/.exec(url);
+    let url = tab.url;
+    let result = /(?<!%[0-9A-Fa-f]{0,2})\d+(?!.*?(?<!%[0-9A-Fa-f]{0,2})\d)/.exec(url);
     if (result) {
       let n = result[0];
       let nLen = n.length;
@@ -11,11 +11,10 @@ onkeyup = e => e.keyCode == 13 && chrome.tabs.query({ active: !0, currentWindow:
       let s0 = url.slice(0, i);
       let s1 = url.slice(i + nLen);
       let isPositive = count > 0;
-      let index = tab.index + isPositive;
-      let tabIds = Array((count = isPositive ? count : -count) + 1);
+      let index = isPositive + tab.index;
       let f = n[0] != "0" ? () => --n : () => (--n + "").padStart(nLen, "0");
-
       n = +n + (isPositive && count + 1);
+      let tabIds = Array((count = isPositive ? count : -count) + 1);
       tabIds[i = 0] = tab.id;
       while (
         tabIds[++i] = chrome.tabs.create({
@@ -23,17 +22,15 @@ onkeyup = e => e.keyCode == 13 && chrome.tabs.query({ active: !0, currentWindow:
           active: !1,
           index
         }),
-        i < count && n
+        n && i < count
       );
-
       tabIds = await Promise.all(tabIds);
       i = 0;
       while (
         tabIds[++i] = tabIds[i].id,
         i < count
       );
-
-      let { groupId } = tab;
+      let groupId = tab.groupId;
       chrome.tabs.group(
         groupId < 0
           ? { tabIds }
